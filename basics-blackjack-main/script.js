897397f78a453479a362821687713ff145a401dc
bw <bwceemail@gmail.com>
2021-09-04 16:08:54 +0800

    manual player blackjack and bust

--- basics-blackjack-main/script.js
+++ basics-blackjack-main/script.js
@@ -94,19 +94,16 @@ let displayArea = document.getElementById("DisplayArea");
 let dlrDisplay = document.getElementById("DlrDisplay");
 let dlrDisplayCards = document.getElementById("DlrDisplayCards");
 let dlrDisplayPoints = document.getElementById("DlrDisplayPoints");
-let dlrDisplayStatus = document.getElementById("DlrDisplayStatus");
-let p1Display = document.getElementById("P1Display");
-let p1DisplayCards = document.getElementById("P1DisplayCards");
-let p1DisplayPoints = document.getElementById("P1DisplayPoints");
-let p1DisplayStatus = document.getElementById("P1DisplayStatus");
+let nowPlayer = document.getElementById("NowPlayer");
+let nowPlayerDisplay = document.getElementById("NowPlayerDisplay");
+let nowPlayerDisplayCards = document.getElementById("NowPlayerDisplayCards");
+let nowPlayerDisplayPoints = document.getElementById("NowPlayerDisplayPoints");
 let p2Display = document.getElementById("P2Display");
 let p2DisplayCards = document.getElementById("P2DisplayCards");
 let p2DisplayPoints = document.getElementById("P2DisplayPoints");
-let p2DisplayStatus = document.getElementById("P2DisplayStatus");
 let p3Display = document.getElementById("P3Display");
 let p3DisplayCards = document.getElementById("P3DisplayCards");
 let p3DisplayPoints = document.getElementById("P3DisplayPoints");
-let p3DisplayStatus = document.getElementById("P3DisplayStatus");
 //*****************************************************************
 //Game Play Codes
 //*****************************************************************
@@ -125,15 +122,17 @@ function dealAgain() {
   const enableList = [hitBtn, stayBtn];
   enableList.forEach((e) => e.removeAttribute("style"));
 
+  const resetList = [rdDlrScore, rdP1Score, rdP2Score, rdP3Score];
+  resetList.forEach((e) => (e.innerHTML = ""));
+
+  const hideList = [p2Display, p3Display];
+  hideList.forEach((e) => e.classList.add("hidden"));
+
   playersData.forEach(function (el) {
     el.Hand = [];
     el.Pts = 0;
     el.Status = "Open";
   });
-  rdDlrScore.innerHTML = "";
-  rdP1Score.innerHTML = "";
-  rdP2Score.innerHTML = "";
-  rdP3Score.innerHTML = "";
 
   deal();
   cal();
@@ -151,9 +150,14 @@ function hit() {
   const playingNow = playersData.find(
     (e) => e.Name === remainingPlayers[0].Name
   );
-  playingNow.Hand.push(playDeck.pop());
-  console.log(playingNow.Name, " hits");
-  cal();
+  if (playingNow.Hand.length >= 5) {
+    alert("You have drawn too many cards. Your turn is over.");
+    stay();
+  } else {
+    playingNow.Hand.push(playDeck.pop());
+    console.log(playingNow.Name, " hits");
+    cal();
+  }
 }
 
 function stay() {
@@ -177,10 +181,6 @@ function stay() {
 ///////////////////////////////////////
 //Helper Functions
 ///////////////////////////////////////
-/* function initialCompare() {
-  cal();
-  check();
-} */
 //Only called for every new round. So start w new deck. Round shld go up by 1 & remainingPlayers reset.
 function deal() {
   playDeck = shuffle(startDeck.slice());
@@ -191,70 +191,36 @@ function deal() {
   }
 }
 
-//If: sum all cards if all players have only 2 cards
-//else: => players have  > 2 cards and Ace counted as 1
-//Get totalPoints and countA for each player
-//2 cards: just use totalPoints
-//3 cards: if >=1 Ace(s) and totalPoints between 18-21 then Ace(s) counts as 11 else  Ace(s) count as 1
-//> 3 cards: Ace(s) count as 1
 function cal() {
+  playersData.forEach(function (elPlayer) {
+    const totalPoints = elPlayer.Hand.reduce((a, b) => {
+      return a + b.Weight;
+    }, 0);
+    let countA = 0;
+    elPlayer.Hand.forEach(function (elHand) {
+      elHand.Value === "A" ? (countA += 1) : countA;
+    });
+    switch (elPlayer.Hand.length) {
+      case 2:
+        elPlayer.Pts = totalPoints;
+        break;
+      case 3:
+        countA >= 1 && totalPoints >= 18 && totalPoints <= 21
+          ? (elPlayer.Pts = totalPoints)
+          : (elPlayer.Pts = totalPoints - 10 * countA);
+        break;
+      default:
+        elPlayer.Pts = totalPoints - 10 * countA;
+    }
+  });
   const dealer = playersData[numPlayers];
   if (dealer.Hand.length === 2 && dealer.Pts >= 21) {
     roundResults();
   } else {
-    playersData.forEach(function (elPlayer) {
-      const totalPoints = elPlayer.Hand.reduce((a, b) => {
-        return a + b.Weight;
-      }, 0);
-      let countA = 0;
-      elPlayer.Hand.forEach(function (elHand) {
-        elHand.Value === "A" ? (countA += 1) : countA;
-      });
-      switch (elPlayer.Hand.length) {
-        case 2:
-          elPlayer.Pts = totalPoints;
-          break;
-        case 3:
-          countA >= 1 && totalPoints >= 18 && totalPoints <= 21
-            ? (elPlayer.Pts = totalPoints)
-            : (elPlayer.Pts = totalPoints - 10 * countA);
-          break;
-        default:
-          elPlayer.Pts = totalPoints - 10 * countA;
-      }
-    });
+    redrawScreen();
+    test();
   }
-  redrawScreen();
-  test();
 }
-//Runs through all players w status "open"
-//Close status of those w 2 cards & Black Jack. If dealer Black Jack, Game over
-//Close status of those who bust or survive 5 cards!
-//If all players closed out, call dealerDraws() to wrap up round
-//Tried using array.find() to look for first object w "open" status but somehow that locked status of first object found to "open"...
-/* function check() {
-  const dealer = playersData[numPlayers];
-   playersData.forEach(function (elPlayer) {
-    if (elPlayer.Status === "Open") {
-      if (elPlayer.Hand.length === 2 && elPlayer.Pts >= 21) {
-        elPlayer.Status = "Closed";
-        if (dealer.Status === "Closed") {
-          finalCompare();
-        }
-      } else if (elPlayer.Pts <= 21 && elPlayer.Hand.length > 4) {
-        elPlayer.Status = "Closed";
-      } else if (elPlayer.Pts > 21) {
-        elPlayer.Status = "Closed";
-      }
-    }
-  });
-  remainingPlayers = playersData.filter((e) => e.Status === "Open");
-  if (remainingPlayers.length === 1) {
-    dealerDraws();
-  }
-  redrawScreen();
-  test();
-} */
 
 //&& comparison to prevent dealer from drawing more than 5 cards and busting
 function dealerDraws() {
@@ -276,7 +242,6 @@ function roundResults() {
 
   for (let i = 0; i < numPlayers; i++) {
     const player = playersData[i];
-    player.Status = "Closed";
     if (player.Pts > 21) {
       if (dealer.Pts <= 21) {
         dealer.Score += 1;
@@ -287,7 +252,10 @@ function roundResults() {
         player.Score += 1;
         thisRdScore[i] += 1;
       } else if (dealer.Pts <= 21) {
-        if (player.Pts > dealer.Pts) {
+        if (dealer.Hand.length === 5) {
+          dealer.Score += 1;
+          thisRdScore[3] += 1;
+        } else if (player.Pts > dealer.Pts) {
           player.Score += 1;
           thisRdScore[i] += 1;
         } else if (player.Pts < dealer.Pts && player.Hand.length === 5) {
@@ -307,8 +275,7 @@ function roundResults() {
   } else if (numPlayers === 2) {
     rdP2Score.innerHTML = thisRdScore[1];
   }
-  redrawScreen();
-  textUpdates.innerHTML = ` <p> <strong> Round ${round} over!</strong><br> Deal Again: Redeal Cards  •  Restart: Restart Game</p>`;
+  endRoundScreen();
 }
 
 //*****************************************************************
@@ -318,7 +285,7 @@ function setupScreen() {
   textUpdates.innerHTML = ` <p> <strong> Round ${round} </strong><br> Hit: Add card  •  Stay: Keep current cards <br> Deal Again: Redeal Cards  •  Restart: Restart Game</p>`;
   header.forEach((e) => e.classList.add("hidden"));
   newGameBox.classList.add("hidden");
-  const unhideList = [othBtnsBox, scoreBox, dlrDisplay, p1Display];
+  const unhideList = [othBtnsBox, scoreBox, dlrDisplay, nowPlayerDisplay];
   unhideList.forEach((e) => e.classList.remove("hidden"));
   const enableList = [hitBtn, stayBtn];
   enableList.forEach((e) => e.removeAttribute("style"));
@@ -330,40 +297,56 @@ function setupScreen() {
     p3Head.innerHTML = "Player 3";
   }
 }
-//Need to have textUpdates so round num can be updated
+
 function redrawScreen() {
   textUpdates.innerHTML = ` <p> <strong> Round ${round} </strong><br> Hit: Add card  •  Stay: Keep current cards <br> Deal Again: Redeal Cards  •  Restart: Restart Game</p>`;
+  remainingPlayers = playersData.filter((e) => e.Status === "Open");
+  const playingNow = playersData.find(
+    (e) => e.Name === remainingPlayers[0].Name
+  );
+
+  /* dlrScore.innerHTML = playersData[numPlayers].Score;
+  p1Score.innerHTML = playersData[0].Score; */
+  nowPlayer.innerHTML = playingNow.Name;
+  dlrDisplayCards.innerHTML = `<i class="${playersData[numPlayers].Hand[1].Suit}">${playersData[numPlayers].Hand[1].Value}</i>`;
+  dlrDisplayPoints.innerHTML = "";
+  nowPlayerDisplayCards.innerHTML = createCardsDisplay(playingNow);
+  nowPlayerDisplayPoints.innerHTML = `Points: ${playingNow.Pts}`;
+}
+function endRoundScreen() {
+  textUpdates.innerHTML = ` <p> <strong> Round ${round} over!</strong><br> Deal Again: Redeal Cards  •  Restart: Restart Game</p>`;
 
   dlrScore.innerHTML = playersData[numPlayers].Score;
   p1Score.innerHTML = playersData[0].Score;
+  nowPlayer.innerHTML = playersData[0].Name;
   dlrDisplayCards.innerHTML = createCardsDisplay(playersData[numPlayers]);
-  p1DisplayCards.innerHTML = createCardsDisplay(playersData[0]);
-  dlrDisplayPoints.innerHTML = playersData[numPlayers].Pts;
-  p1DisplayPoints.innerHTML = playersData[0].Pts;
-  dlrDisplayStatus.innerHTML = "Status: Open";
-  p1DisplayStatus.innerHTML = "Status: " + playersData[0].Status;
+  nowPlayerDisplayCards.innerHTML = createCardsDisplay(playersData[0]);
+  dlrDisplayPoints.innerHTML = `Points: ${playersData[numPlayers].Pts}`;
+  nowPlayerDisplayPoints.innerHTML = `Points: ${playersData[0].Pts}`;
 
   if (numPlayers === 2) {
+    const unhideList = [p2Display];
+    unhideList.forEach((e) => e.classList.remove("hidden"));
     p2Score.innerHTML = playersData[1].Score;
     p2DisplayCards.innerHTML = createCardsDisplay(playersData[1]);
-    p2DisplayPoints.innerHTML = playersData[1].Pts;
-    p2DisplayStatus.innerHTML = "Status: " + playersData[1].Status;
+    p2DisplayPoints.innerHTML = `Points: ${playersData[1].Pts}`;
   } else if (numPlayers === 3) {
+    const unhideList = [p2Display, p3Display];
+    unhideList.forEach((e) => e.classList.remove("hidden"));
     p2Score.innerHTML = playersData[1].Score;
     p2DisplayCards.innerHTML = createCardsDisplay(playersData[1]);
-    p2DisplayPoints.innerHTML = playersData[1].Pts;
-    p2DisplayStatus.innerHTML = "Status: " + playersData[1].Status;
+    p2DisplayPoints.innerHTML = `Points: ${playersData[1].Pts}`;
+
     p3Score.innerHTML = playersData[2].Score;
     p3DisplayCards.innerHTML = createCardsDisplay(playersData[2]);
-    p3DisplayPoints.innerHTML = playersData[2].Pts;
-    p3DisplayStatus.innerHTML = "Status: " + playersData[2].Status;
+    p3DisplayPoints.innerHTML = `Points: ${playersData[2].Pts}`;
   }
 }
 
 function createCardsDisplay(player) {
-  let result = `<i class="${player.Hand[0].Suit}">${player.Hand[0].Weight}</i>`;
+  let result = `<i class="${player.Hand[0].Suit}">${player.Hand[0].Value}</i>`;
   for (let i = 1; i < player.Hand.length; i++) {
-    result += `<i class="${player.Hand[i].Suit}">-${player.Hand[i].Weight}</i>`;
+    result += `<i class="${player.Hand[i].Suit}">-${player.Hand[i].Value}</i>`;
   }
   return result;
 }
@@ -375,7 +358,14 @@ function resetScreen() {
   const unhideList = [newGameBox, hitBtn, stayBtn];
   unhideList.forEach((e) => e.classList.remove("hidden"));
 
-  const hideList = [othBtnsBox, scoreBox, dlrDisplay, p1Display];
+  const hideList = [
+    othBtnsBox,
+    scoreBox,
+    dlrDisplay,
+    nowPlayerDisplay,
+    p2Display,
+    p3Display,
+  ];
   hideList.forEach((e) => e.classList.add("hidden"));
 
   playersSelected.selectedIndex = 0; //reset drop down list to 1
@@ -393,16 +383,13 @@ function resetScreen() {
     rdP3Score,
     dlrDisplayCards,
     dlrDisplayPoints,
-    dlrDisplayStatus,
-    p1DisplayCards,
-    p1DisplayPoints,
-    p1DisplayStatus,
+    nowPlayer,
+    nowPlayerDisplayCards,
+    nowPlayerDisplayPoints,
     p2DisplayCards,
     p2DisplayPoints,
-    p2DisplayStatus,
     p3DisplayCards,
     p3DisplayPoints,
-    p3DisplayStatus,
   ];
 
   resetList.forEach((e) => (e.innerHTML = ""));
